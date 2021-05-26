807e60dcee1c9833fb8c12c62b26268c92448752
Vlad <s237362@studenti.polito.it>
2021-05-26 18:30:44 +0200

    bozza minimappa

---
+++ GameDesign_UnityProject/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Player;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 CameraFollowPosition = Player.position;
+        CameraFollowPosition.y = transform.position.y;
+        transform.position = CameraFollowPosition;
+
+        transform.rotation = Quaternion.Euler(90f, Player.eulerAngles.y, 0f);
+    }
+}
