ecddcae0957bb89cc25b88dc31c8f9cd493743fb
Giorgio <giorgios94@hotmail.it>
2021-06-29 19:56:43 +0200

    Personaggio e camera funzionanti, sia con controller che tastiera

--- GameDesign_UnityProject/Assets/Character/Scirpts/CharacterController.cs
+++ GameDesign_UnityProject/Assets/Character/Scirpts/CharacterController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class ZZCharacterController : MonoBehaviour
 {
     private Animator animator;
     private bool isMoving, isRunning, isJumping;
