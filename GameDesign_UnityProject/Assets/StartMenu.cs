88f40a51c0a148a3797cb8dca4c2249b69c84c7d
Vlad <s237362@studenti.polito.it>
2021-07-07 16:07:02 +0200

    start menu ok

--- GameDesign_UnityProject/Assets/StartMenu.cs
+++ GameDesign_UnityProject/Assets/StartMenu.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    public GameObject pauseFirstButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // tolgo preventivamente qualsiasi selezione rimasta su qualche oggetto
+        EventSystem.current.SetSelectedGameObject(null);
+        // ora posso selezionare in oggetto
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton);   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("ScenaPrincipale");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
