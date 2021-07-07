a6380678ae282d25cbf8a9b821e77199edf763e3
Filippo <f.rampolla98@gmail.com>
2021-07-08 00:59:44 +0200

    dog

---
+++ GameDesign_UnityProject/Assets/active_robot.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class active_robot : MonoBehaviour
+{
+    public GameObject canvas;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            canvas.SetActive(true);
+
+
+        }
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            canvas.SetActive(false);
+           
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            FindObjectOfType<LevelLoader>().LoadNextLevelTurin();
+        }
+    }
+}
