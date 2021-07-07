491ce960784e9149063e2bc38e80ecbd1bb0bbdd
Vlad <s237362@studenti.polito.it>
2021-07-07 18:39:33 +0200

    audio ok

--- GameDesign_UnityProject/Assets/StartMenu.cs
+++ GameDesign_UnityProject/Assets/StartMenu.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class StartMenu : MonoBehaviour
 {
     public GameObject pauseFirstButton;
+
+    public GameObject canvasStart;
+
+    public GameObject canvasOptions;
+
+    public GameObject optionsFirstButton;
+
+    public AudioMixer audioMixer;
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,5 +44,30 @@ public class StartMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void fromStartToOptions()
+    {
+        canvasStart.SetActive(false);
+        canvasOptions.SetActive(true);
+        // tolgo preventivamente qualsiasi selezione rimasta su qualche oggetto
+        EventSystem.current.SetSelectedGameObject(null);
+        // ora posso selezionare in oggetto
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton); 
+    }
+
+    public void fromOptionsToStart()
+    {
+        canvasStart.SetActive(true);
+        canvasOptions.SetActive(false);
+        // tolgo preventivamente qualsiasi selezione rimasta su qualche oggetto
+        EventSystem.current.SetSelectedGameObject(null);
+        // ora posso selezionare in oggetto
+        EventSystem.current.SetSelectedGameObject(pauseFirstButton); 
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volumeMaster", volume);
     }
 }
