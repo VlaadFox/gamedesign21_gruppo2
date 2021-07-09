6f94124c1a7ada86853309e7ffdc0370261e8a36
Filippo <f.rampolla98@gmail.com>
2021-07-09 08:43:05 +0200

    sotteranoe2

---
+++ GameDesign_UnityProject/Assets/active_quadro.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class active_quadro : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject light;
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            canvas.SetActive(true);
+
+
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        canvas.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                light.SetActive(true);
+            }
+        }
+    }
+}
